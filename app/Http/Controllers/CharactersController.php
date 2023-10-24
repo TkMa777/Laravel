6dcb29bccf1c77628fc40cd51981a24aec97112f
Mkrtchyan Tatshat <121193072+TkMa777@users.noreply.github.com>
2023-10-24 19:43:16 +0200

    Controller

--- app/Http/Controllers/CharactersController.php
+++ app/Http/Controllers/CharactersController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Character;
 use Illuminate\Http\Request;
+
+
+/*
+ * Depuis mon base de données,
+ * je recoupere la liste des personnages de sexe féminin
+ * et de l’espèce humaine que vous passerez à la vue blade
+ */
 
 class CharactersController extends Controller
 {
     public function index()
     {
-        return view('characters');
+        $femaleCharacters = Character::where('gender', 'Female')
+            -> where ('species', 'Human')
+            -> get();
+        return view('characters' , ['characters' => $femaleCharacters]);
     }
 }
